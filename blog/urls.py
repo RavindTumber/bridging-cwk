e504957f17ed8288e5f0dc32db098d4cf4e1df1e
rxt845 <rxt845@student.bham.ac.uk>
2020-06-23 12:40:58 +0100

    Navbar added to all pages

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 # URL configuration for our blog web app
 
+app_name = 'blog'
 urlpatterns = [
     path('', views.post_list, name='post_list'),
 ]
--- cv/urls.py
+++ cv/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 # URL configuration for our CV web app
 
+app_name = 'cv'
 urlpatterns = [
     path('', views.display_cv, name='display_cv'),
 ]
