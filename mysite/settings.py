a3a35f8e34ea90dd44d83e85c4bcdf2d2a58e6e7
rxt845 <rxt845@student.bham.ac.uk>
2020-07-07 22:47:16 +0100

    Integrated web app with django-summernote

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,9 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # local apps
     'blog.apps.BlogConfig',
     'cv.apps.CvConfig',
     'landing.apps.LandingConfig',
+
+    # third party
+    'django_summernote',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,9 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('cv/', include('cv.urls')),
     path('', include('landing.urls')),
+    path('summernote/', include('django_summernote.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
