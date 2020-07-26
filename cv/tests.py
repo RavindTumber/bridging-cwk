032822e44861e7237f77c182b6bff61e4edc7966
rxt845 <rxt845@student.bham.ac.uk>
2020-07-26 23:24:37 +0100

    Refactored test by using Django's test client

--- cv/tests.py
+++ cv/tests.py
@@ -6,12 +6,6 @@ from cv.views import display_cv
 
 class CVPageTest(TestCase):
 
-    def test_root_url_resolves_to_cv_page_view(self):
-        found = resolve('/cv/')  
-        self.assertEqual(found.func, display_cv)
-
-    def test_cv_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = display_cv(request)
-        html = response.content.decode('utf8')
-        self.assertIn('<title>CV</title>', html)
+    def test_uses_cv_template(self):
+        response = self.client.get('/cv/')
+        self.assertTemplateUsed(response, 'cv/cv.html')
